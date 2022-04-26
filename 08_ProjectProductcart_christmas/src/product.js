1a27367aab2e5e88607d870dded2250c4d73336c
sor-ee <68268283+sor-ee@users.noreply.github.com>
2022-04-26 22:06:09 +0700

    Add files via upload

---
+++ 08_ProjectProductcart_christmas/src/product.js
@@ -0,0 +1,53 @@
+//สร้างproductที่จะแสดงในหน้าเว็บไซต์
+let product = [
+  {
+    img: "src/assets/images/icon/n1.png",
+    productId: "n1",
+    productName: "MSI GE76 Raider",
+    productDesc: "Spec - สูง",
+    price: 51990,
+    total: 25
+  },
+  {
+    img: "src/assets/images/icon/n2.png",
+    productId: "n1",
+    productName: "Lenovo LEGION Y540-81SY00ARTA",
+    productDesc: "Spec - กลาง    ",
+    price: 15710,
+    total: 28
+  },
+  {
+    img: "src/assets/images/icon/n3.png",
+    productId: "n3",
+    productName: "GARLenovo ThinkBook 14-20VD0055TA  MIN2",
+    productDesc: "Spec - ต่ำ",
+    price: 10990,
+    total: 18
+  },
+  {
+    img: "src/assets/images/icon/k1.png",
+    productId: "k1",
+    productName: "Panasonic GM1",
+    productDesc: "Spec - ต่ำ",
+    price: 3400,
+    total: 30
+  },
+  {
+    img: "src/assets/images/icon/k2.png",
+    productId: "k1",
+    productName: "Nikon D5600 Kit AF-P 18-55 mm.-G",
+    productDesc: "Spec - กลาง",
+    price: 4200,
+    total: 25
+  },
+  {
+    img: "src/assets/images/icon/k3.png",
+    productId: "k1",
+    productName: "Canon Camera EOS 90D kit 18-55 mm    ",
+    productDesc: "Spec - สูง",
+    price: 10200,
+    total: 25
+  },
+
+];
+export { product };
